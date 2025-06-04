6503638272736b34a766e7adbbbc8f9dd5faaa53
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2025-06-05 01:23:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function highlight() {
     //Write your code here
+	const ele = document.getElementById("color");
+	const boldWorld = ele.getElementsByTagName("strong");
 
+	for(let i=0;i<boldWorld.length;i++){
+		boldWorld[i].style.color="rgb(0, 128, 0)";
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
+	const ele = document.getElementById("color");
+	const boldWorld = ele.getElementsByTagName("strong");
 
-    
+	for(let i=0;i<boldWorld.length;i++){
+		boldWorld[i].style.color="rgb(0, 0, 0)";
+	}
 }
